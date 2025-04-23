92416c02b1aa3569c35d123eb48e1a409e6dcaca
Vaibhav khandelwal <145020241+Vaibhavtech2006@users.noreply.github.com>
2025-04-23 20:20:06 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Vaibhav's Portfolio",
+  title: "Agent AI",
   description: "learning nextjs through project building",
 };
 
